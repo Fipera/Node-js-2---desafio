d232ca0e625f6592d4ca284ffee2658b2cb1b5f4
Filipe Bello <filipeg.bello@gmail.com>
2024-09-03 23:04:32 -0300

    first commit

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,12 @@
+import { Knex } from 'knex'
+
+declare module "knex/types/tables" {
+    export interface Tables {
+        users: {
+            id: string
+            name: string
+            password: string
+            session_id? : string
+        }
+    }
+}
